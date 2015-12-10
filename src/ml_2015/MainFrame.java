91d545df76861c961936b744c315b3d40850d614
eirinimp <eirini_mp94@hotmail.com>
2015-12-10 16:44:26 +0200

    test

--- src/ml_2015/MainFrame.java
+++ src/ml_2015/MainFrame.java
@@ -51,7 +51,7 @@ public class MainFrame {
 		JButton btnNewButton_1 = new JButton("New button");
 		panel.add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("New bu");
+		JButton btnNewButton_2 = new JButton("New button");
 		frame.getContentPane().add(btnNewButton_2, BorderLayout.WEST);
 	}
 

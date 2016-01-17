071c982a9972fd0026015f91cb41ed4aebc30109
georgekapai <georgekapai@hotmail.com>
2016-01-17 19:00:33 +0200

    test

--- src/ml_2015/MainFrame.java
+++ src/ml_2015/MainFrame.java
@@ -103,6 +103,192 @@ public class MainFrame {
 		passwordField.setBounds(400, 120, 130, 25);
 		frmUstore.getContentPane().add(passwordField);
 		
+		JButton button_00 = new JButton("");
+		button_00.setFont(new Font("Arial", Font.PLAIN, 12));
+		button_00.setIcon(new ImageIcon("src\\icons\\logo.png"));
+		button_00.setBackground(Color.WHITE);
+		button_00.setBounds(0, 0, 50, 50);
+		frmUstore.getContentPane().add(button_00);
+		button_00.setVisible(false);
+
+		JLabel label_0 = new JLabel("  ");
+		label_0.setFont(new Font("Arial", Font.BOLD, 12));
+		label_0.setBounds(1050, 20, 80, 15);
+		frmUstore.getContentPane().add(label_0);
+		label_0.setVisible(false);
+
+		JButton button_03 = new JButton("");
+		button_03.setIcon(new ImageIcon("src\\icons\\power.png"));
+		button_03.setBackground(Color.WHITE);
+		button_03.setMaximumSize(new Dimension(20, 20));
+		button_03.setFont(new Font("Arial", Font.PLAIN, 10));
+		button_03.setBounds(1145, 0, 50, 50);
+		frmUstore.getContentPane().add(button_03);
+		button_03.setVisible(false);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(0, 51, 1200, 7);
+		frmUstore.getContentPane().add(separator);
+		separator.setVisible(false);
+		
+		JSeparator separator_2 = new JSeparator();
+		separator_2.setBounds(0, 85, 1500, 7);
+		frmUstore.getContentPane().add(separator_2);
+		separator_2.setVisible(false);
+		
+		JButton button_1 = new JButton("");
+		button_1.setIcon(new ImageIcon("src\\icons\\add.png"));
+		button_1.setBackground(Color.WHITE);
+		button_1.setBounds(100, 180, 125, 125);
+		frmUstore.getContentPane().add(button_1);
+		button_1.setVisible(false);
+		
+		JButton button_2 = new JButton("");
+		button_2.setIcon(new ImageIcon("src\\icons\\screen.png"));
+		button_2.setBackground(Color.WHITE);
+		button_2.setBounds(305, 180, 125, 125);
+		frmUstore.getContentPane().add(button_2);
+		button_2.setVisible(false);
+
+		JButton button_3 = new JButton("");
+		button_3.setIcon(new ImageIcon("src\\icons\\edit.png"));
+		button_3.setBackground(Color.WHITE);
+		button_3.setBounds(510, 180, 125, 125);
+		frmUstore.getContentPane().add(button_3);
+		button_3.setVisible(false);
+
+		JButton button_4 = new JButton("");
+		button_4.setIcon(new ImageIcon("src\\icons\\search_1.png"));
+		button_4.setBackground(Color.WHITE);
+		button_4.setBounds(715, 180, 125, 125);
+		frmUstore.getContentPane().add(button_4);
+		button_4.setVisible(false);
+
+		JButton button_5 = new JButton("");
+		button_5.setIcon(new ImageIcon("src\\icons\\statistics.png"));
+		button_5.setBackground(Color.WHITE);
+		button_5.setBounds(920, 180, 125, 125);
+		frmUstore.getContentPane().add(button_5);
+		button_5.setVisible(false);
+		
+		JLabel label_1 = new JLabel("дглиоуяциа");
+		label_1.setFont(new Font("Arial", Font.PLAIN, 15));
+		label_1.setBounds(120, 310, 150, 70);
+		frmUstore.getContentPane().add(label_1);
+		label_1.setVisible(false);
+
+		JLabel label_12 = new JLabel("меоу пяозомтос");
+		label_12.setFont(new Font("Arial", Font.PLAIN, 15));
+		label_12.setBounds(98, 330, 150, 70);
+		frmUstore.getContentPane().add(label_12);
+		label_12.setVisible(false);
+
+		JLabel label_2 = new JLabel("пяобокг");
+		label_2.setFont(new Font("Arial", Font.PLAIN, 15));
+		label_2.setBounds(330, 300, 150, 70);
+		frmUstore.getContentPane().add(label_2);
+		label_2.setVisible(false);
+
+		JLabel label_22 = new JLabel("апохелатос");
+		label_22.setFont(new Font("Arial", Font.PLAIN, 15));
+		label_22.setBounds(318, 320, 150, 70);
+		frmUstore.getContentPane().add(label_22);
+		label_22.setVisible(false);
+
+		JLabel label_23 = new JLabel("пяозомтос");
+		label_23.setFont(new Font("Arial", Font.PLAIN, 15));
+		label_23.setBounds(325, 340, 150, 70);
+		frmUstore.getContentPane().add(label_23);
+		label_23.setVisible(false);
+
+		JLabel label_3 = new JLabel("епенеяцасиа");
+		label_3.setFont(new Font("Arial", Font.PLAIN, 15));
+		label_3.setBounds(525, 300, 150, 70);
+		frmUstore.getContentPane().add(label_3);
+		label_3.setVisible(false);
+
+		JLabel label_32 = new JLabel("апохелатос");
+		label_32.setFont(new Font("Arial", Font.PLAIN, 15));
+		label_32.setBounds(521, 320, 150, 70);
+		frmUstore.getContentPane().add(label_32);
+		label_32.setVisible(false);
+
+		JLabel label_33 = new JLabel("пяозомтос");
+		label_33.setFont(new Font("Arial", Font.PLAIN, 15));
+		label_33.setBounds(528, 340, 150, 70);
+		frmUstore.getContentPane().add(label_33);
+		label_33.setVisible(false);
+
+		JLabel label_4 = new JLabel("амафгтгсг ста");
+		label_4.setFont(new Font("Arial", Font.PLAIN, 15));
+		label_4.setBounds(720, 310, 150, 70);
+		frmUstore.getContentPane().add(label_4);
+		label_4.setVisible(false);
+
+		JLabel label_42 = new JLabel("апохелата");
+		label_42.setFont(new Font("Arial", Font.PLAIN, 15));
+		label_42.setBounds(730, 330, 150, 70);
+		frmUstore.getContentPane().add(label_42);
+		label_42.setVisible(false);
+
+		JLabel label_5 = new JLabel("елжамисг");
+		label_5.setFont(new Font("Arial", Font.PLAIN, 15));
+		label_5.setBounds(945, 300, 150, 70);
+		frmUstore.getContentPane().add(label_5);
+		label_5.setVisible(false);
+
+		JLabel label_52 = new JLabel("статистийым");
+		label_52.setFont(new Font("Arial", Font.PLAIN, 15));
+		label_52.setBounds(938, 320, 150, 70);
+		frmUstore.getContentPane().add(label_52);
+		label_52.setVisible(false);
+
+		JLabel label_53 = new JLabel("стоивеиым");
+		label_53.setFont(new Font("Arial", Font.PLAIN, 15));
+		label_53.setBounds(945, 340, 150, 70);
+		frmUstore.getContentPane().add(label_53);
+		label_53.setVisible(false);
+		
+		JLabel label_task_1 = new JLabel("пяобокг апохелатос пяозомтос");
+		label_task_1.setBounds(0, 55, 500, 25);
+		label_task_1.setBackground(Color.DARK_GRAY);
+		label_task_1.setForeground(Color.DARK_GRAY);
+		label_task_1.setFont(new Font("Arial", Font.PLAIN, 20));
+		frmUstore.getContentPane().add(label_task_1);
+		label_task_1.setVisible(false);
+		
+		JLabel label_task_2 = new JLabel("пяобокг апохелатос пяозомтос");
+		label_task_2.setBounds(0, 55, 500, 25);
+		label_task_2.setBackground(Color.DARK_GRAY);
+		label_task_2.setForeground(Color.DARK_GRAY);
+		label_task_2.setFont(new Font("Arial", Font.PLAIN, 20));
+		frmUstore.getContentPane().add(label_task_2);
+		label_task_2.setVisible(false);
+		
+		JLabel label_task_3 = new JLabel("пяобокг апохелатос пяозомтос");
+		label_task_3.setBounds(0, 55, 500, 25);
+		label_task_3.setBackground(Color.DARK_GRAY);
+		label_task_3.setForeground(Color.DARK_GRAY);
+		label_task_3.setFont(new Font("Arial", Font.PLAIN, 20));
+		frmUstore.getContentPane().add(label_task_3);
+		label_task_3.setVisible(false);
+		
+		JLabel label_task_4 = new JLabel("пяобокг апохелатос пяозомтос");
+		label_task_4.setBounds(0, 55, 500, 25);
+		label_task_4.setBackground(Color.DARK_GRAY);
+		label_task_4.setForeground(Color.DARK_GRAY);
+		label_task_4.setFont(new Font("Arial", Font.PLAIN, 20));
+		frmUstore.getContentPane().add(label_task_4);
+		label_task_4.setVisible(false);
+		
+		JLabel label_task_5 = new JLabel("пяобокг апохелатос пяозомтос");
+		label_task_5.setBounds(0, 55, 500, 25);
+		label_task_5.setBackground(Color.DARK_GRAY);
+		label_task_5.setForeground(Color.DARK_GRAY);
+		label_task_5.setFont(new Font("Arial", Font.PLAIN, 20));
+		frmUstore.getContentPane().add(label_task_5);
+		label_task_5.setVisible(false);
+		
 		JButton btnLogIn = new JButton("сумдесг");
 		btnLogIn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -114,124 +300,30 @@ public class MainFrame {
 					txtUsername.setVisible(false);
 					passwordField.setVisible(false);
 					btnLogIn.setVisible(false);
-					JButton button_00 = new JButton("");
-					button_00.setFont(new Font("Arial", Font.PLAIN, 12));
-					button_00.setIcon(new ImageIcon("src\\icons\\logo.png"));
-					button_00.setBackground(Color.WHITE);
-					button_00.setBounds(0, 0, 50, 50);
-					frmUstore.getContentPane().add(button_00);
-
-					JLabel label_0 = new JLabel("  ");
-					label_0.setFont(new Font("Arial", Font.BOLD, 12));
-					label_0.setBounds(1050, 20, 80, 15);
-					frmUstore.getContentPane().add(label_0);
-
-					JButton button_03 = new JButton("");
-					button_03.setIcon(new ImageIcon("src\\icons\\power.png"));
-					button_03.setBackground(Color.WHITE);
-					button_03.setMaximumSize(new Dimension(20, 20));
-					button_03.setFont(new Font("Arial", Font.PLAIN, 10));
-					button_03.setBounds(1134, 0, 50, 50);
-					frmUstore.getContentPane().add(button_03);
-
-					JSeparator separator = new JSeparator();
-					separator.setBounds(0, 51, 1200, 7);
-					frmUstore.getContentPane().add(separator);
+					button_00.setVisible(true);
+					label_0.setVisible(true);
+					button_03.setVisible(true);
+					separator.setVisible(true);
+					button_1.setVisible(true);
+					button_2.setVisible(true);
+					button_3.setVisible(true);
+					button_4.setVisible(true);
+					button_5.setVisible(true);
+					label_1.setVisible(true);
+					label_12.setVisible(true);
+					label_2.setVisible(true);
+					label_22.setVisible(true);
+					label_23.setVisible(true);
+					label_3.setVisible(true);
+					label_32.setVisible(true);
+					label_33.setVisible(true);
+					label_4.setVisible(true);
+					label_42.setVisible(true);
+					label_5.setVisible(true);
+					label_52.setVisible(true);
+					label_53.setVisible(true);
 					
-					JButton button_1 = new JButton("");
-					button_1.setIcon(new ImageIcon("src\\icons\\add.png"));
-					button_1.setBackground(Color.WHITE);
-					button_1.setBounds(100, 180, 125, 125);
-					frmUstore.getContentPane().add(button_1);
 					
-					JButton button_2 = new JButton("");
-					button_2.setIcon(new ImageIcon("src\\icons\\screen.png"));
-					button_2.setBackground(Color.WHITE);
-					button_2.setBounds(305, 180, 125, 125);
-					frmUstore.getContentPane().add(button_2);
-
-					JButton button_3 = new JButton("");
-					button_3.setIcon(new ImageIcon("src\\icons\\edit.png"));
-					button_3.setBackground(Color.WHITE);
-					button_3.setBounds(510, 180, 125, 125);
-					frmUstore.getContentPane().add(button_3);
-
-					JButton button_4 = new JButton("");
-					button_4.setIcon(new ImageIcon("src\\icons\\search_1.png"));
-					button_4.setBackground(Color.WHITE);
-					button_4.setBounds(715, 180, 125, 125);
-					frmUstore.getContentPane().add(button_4);
-
-					JButton button_5 = new JButton("");
-					button_5.setIcon(new ImageIcon("src\\icons\\statistics.png"));
-					button_5.setBackground(Color.WHITE);
-					button_5.setBounds(920, 180, 125, 125);
-					frmUstore.getContentPane().add(button_5);
-					
-					JLabel label_1 = new JLabel("дглиоуяциа");
-					label_1.setFont(new Font("Arial", Font.PLAIN, 15));
-					label_1.setBounds(120, 310, 150, 70);
-					frmUstore.getContentPane().add(label_1);
-
-					JLabel label_12 = new JLabel("меоу пяозомтос");
-					label_12.setFont(new Font("Arial", Font.PLAIN, 15));
-					label_12.setBounds(98, 330, 150, 70);
-					frmUstore.getContentPane().add(label_12);
-
-					JLabel label_2 = new JLabel("пяобокг");
-					label_2.setFont(new Font("Arial", Font.PLAIN, 15));
-					label_2.setBounds(330, 300, 150, 70);
-					frmUstore.getContentPane().add(label_2);
-
-					JLabel label_22 = new JLabel("апохелатос");
-					label_22.setFont(new Font("Arial", Font.PLAIN, 15));
-					label_22.setBounds(318, 320, 150, 70);
-					frmUstore.getContentPane().add(label_22);
-
-					JLabel label_23 = new JLabel("пяозомтос");
-					label_23.setFont(new Font("Arial", Font.PLAIN, 15));
-					label_23.setBounds(325, 340, 150, 70);
-					frmUstore.getContentPane().add(label_23);
-
-					JLabel label_3 = new JLabel("епенеяцасиа");
-					label_3.setFont(new Font("Arial", Font.PLAIN, 15));
-					label_3.setBounds(525, 300, 150, 70);
-					frmUstore.getContentPane().add(label_3);
-
-					JLabel label_32 = new JLabel("апохелатос");
-					label_32.setFont(new Font("Arial", Font.PLAIN, 15));
-					label_32.setBounds(521, 320, 150, 70);
-					frmUstore.getContentPane().add(label_32);
-
-					JLabel label_33 = new JLabel("пяозомтос");
-					label_33.setFont(new Font("Arial", Font.PLAIN, 15));
-					label_33.setBounds(528, 340, 150, 70);
-					frmUstore.getContentPane().add(label_33);
-
-					JLabel label_4 = new JLabel("амафгтгсг ста");
-					label_4.setFont(new Font("Arial", Font.PLAIN, 15));
-					label_4.setBounds(720, 310, 150, 70);
-					frmUstore.getContentPane().add(label_4);
-
-					JLabel label_42 = new JLabel("апохелата");
-					label_42.setFont(new Font("Arial", Font.PLAIN, 15));
-					label_42.setBounds(730, 330, 150, 70);
-					frmUstore.getContentPane().add(label_42);
-
-					JLabel label_5 = new JLabel("елжамисг");
-					label_5.setFont(new Font("Arial", Font.PLAIN, 15));
-					label_5.setBounds(945, 300, 150, 70);
-					frmUstore.getContentPane().add(label_5);
-
-					JLabel label_52 = new JLabel("статистийым");
-					label_52.setFont(new Font("Arial", Font.PLAIN, 15));
-					label_52.setBounds(938, 320, 150, 70);
-					frmUstore.getContentPane().add(label_52);
-
-					JLabel label_53 = new JLabel("стоивеиым");
-					label_53.setFont(new Font("Arial", Font.PLAIN, 15));
-					label_53.setBounds(945, 340, 150, 70);
-					frmUstore.getContentPane().add(label_53);
 					
 					button_1.addActionListener(new ActionListener(){
 						public void actionPerformed(ActionEvent e){
@@ -253,17 +345,9 @@ public class MainFrame {
 							label_5.setVisible(false);
 							label_52.setVisible(false);
 							label_53.setVisible(false);
+							label_task_1.setVisible(true);
 							
-							JLabel label_task = new JLabel("дглиоуяциа меоу пяозомтос");
-							label_task.setBackground(Color.DARK_GRAY);
-							label_task.setForeground(Color.DARK_GRAY);
-							label_task.setFont(new Font("Arial", Font.PLAIN, 20));
-							label_task.setBounds(10, 55, 500, 25);
-							frmUstore.getContentPane().add(label_task);
-							
-							JSeparator separator_2 = new JSeparator();
-							separator_2.setBounds(0, 85, 1500, 7);
-							frmUstore.getContentPane().add(separator_2);
+							separator_2.setVisible(true);
 							
 							JLabel new_product_label_1 = new JLabel("тъТКОР");
 							new_product_label_1.setFont(new Font("Arial", Font.PLAIN, 15));
@@ -402,16 +486,10 @@ public class MainFrame {
 							label_52.setVisible(false);
 							label_53.setVisible(false);
 							
-							JLabel label_task = new JLabel("пяобокг апохелатос пяозомтос");
-							label_task.setBounds(0, 55, 500, 25);
-							label_task.setBackground(Color.DARK_GRAY);
-							label_task.setForeground(Color.DARK_GRAY);
-							label_task.setFont(new Font("Arial", Font.PLAIN, 20));
-							frmUstore.getContentPane().add(label_task);
 							
-							JSeparator separator_2 = new JSeparator();
-							separator_2.setBounds(0, 85, 1200, 7);
-							frmUstore.getContentPane().add(separator_2);
+							label_task_2.setVisible(true);
+							
+							separator_2.setVisible(true);
 							
 							JButton button_02 = new JButton("");
 							button_02.setIcon(new ImageIcon("src\\icons\\back.png"));
